be85cf01871bbbcaed3224b8ba60f130b2830f78
Ragib Hasan <hragib1@gmail.com>
2026-06-11 21:14:38 +0530

    chores: due to error in name.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcrypt");
 
 require("dotenv").config();
 
